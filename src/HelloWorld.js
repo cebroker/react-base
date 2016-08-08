10bb4f29805621be9f59218df86127a37debbf68
Fernando Caldera <fcaldera@cebroker.com>
2016-08-08 17:45:38 -0500

    nivigable stalesscomponent added

--- src/HelloWorld.js
+++ src/HelloWorld.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 
-class HelloWorld extends Component {
-	render() {
-		return (
-			
-		)
-	}
+const HelloWorld = () => {
+  return (
+    <div>Say hello to the world from your first stateless component at <code>src/HelloWorld</code></div>
+  )
 }
+
+export default HelloWorld;
